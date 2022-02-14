9abf6f18580fd6076daa3a1003692491249ac958
suhyeon <minsoohyeon1234@gmail.com>
2022-02-14 17:27:46 +0900

    feat: 정답일경우 게임을 재시작 할 것인지 확인.
    1번 =>  재시작
    2번 =>  게임 종료

--- index.js
+++ index.js
@@ -20,20 +20,31 @@ function userInput() {
     let input
     while (flag == true) {
         input = readlineSync.question("# 숫자를 입력해주세요 : ");
-        console.log(input)
         input = input.split('').map(element => parseInt(element)); // 1 2 3 4
         if (input.length > 3 || input.length == 0 || [...new Set(input)].length < 3) {
             console.log("다시 입력해 주세요")
             flag = true
             continue
         }
-        console.log("올바른 숫자형식입니다", input)
         flag = false
     }
     return input
 }
 
+function restart() {
+    let restartFlag
 
+    restartFlag = readlineSync.question("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    if (restartFlag == 1) {
+        startGame()
+    } else if (restartFlag == 2) {
+        console.log("게임을 종료합니다.")
+    } else {
+        console.log("다시 입력해 주세요.")
+        restart()
+    }
+}
 function compareValue(computerNumber, input) {
     let strike
     let ball
@@ -41,11 +52,14 @@ function compareValue(computerNumber, input) {
     strike = 0
     ball = 0
     strike = checkStrike(strike, computerNumber, input)
-    console.log("strike", strike)
     ball = checkBall(strike, ball, computerNumber, input)
-    console.log("ball", ball)
+
     if (strike == 3) {
         console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        console.log()
+        console.log("====================================================================================")
+        console.log("====================================================================================")
+        restart()
     } else if (strike != 0 && ball != 0) {
         console.log(`${strike} 스트라이크 ${ball} 볼`)
     } else if (strike == 0 && ball != 0) {
@@ -74,11 +88,10 @@ function checkBall(strike, ball, computerNumber, input) {
 
 function startGame() {
     computerNumber = makeComputerNumber()
-    console.log(computerNumber)
+    // console.log(computerNumber)
     strike = 0
     while(strike != 3){
         input = userInput()
-        console.log(input)
         strike = compareValue(computerNumber, input)
     }
 
