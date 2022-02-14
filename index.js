6bf524f48c4c110a2384dc6a32071e7d4acbb9d4
suhyeon <minsoohyeon1234@gmail.com>
2022-02-14 16:05:25 +0900

    feat: 사용자 값 입력받고 유효성 체크

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const readlineSync = require('readline-sync');
+
 function makeComputerNumber(){
     let computerNumber = []
     let i = 0
@@ -13,10 +15,26 @@ function makeComputerNumber(){
     }
     return computerNumber
 }
-
+function userInput(){
+    let flag = true
+    let input
+    while(flag == true){
+        input = readlineSync.question("# 숫자를 입력해주세요 : ");
+        console.log(input)
+        input = input.split('').map(element => parseInt(element)); // 1 2 3 4
+        if(input.length > 3 || input.length == 0 || [...new Set(input)].length < 3) {
+            console.log("다시 입력해 주세요")
+            flag = true
+            continue
+        }
+        console.log("올바른 숫자형식입니다",input)
+        flag = false
+    }
+}
 function solution() {
     computerNumber = makeComputerNumber()
     console.log(computerNumber)
+    userInput()
 
 }
 solution()
